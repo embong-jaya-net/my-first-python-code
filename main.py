675e1d1197346ef1bfb52a18332ab82aebeaf8dd
togarmedan <jauhari.andi@gmail.com>
2021-10-29 21:51:09 +0700

    contoh program pertama : sequential dan percabangan

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-'''
-ini program pertama saya
-'''
+# KONSTRUKSI DASAR PYTHON
+# SEQUENTIAL: Eksekusi berurutan
+print('Ibu memerintahkan Budi pergi ke toko membeli 1 botol susu atau jika ada telur makan beli 1 botol susu dan 6 butir telur')
+print('Budi menjawab, "Siap Mami"')
+print('Budi pergi ke toko')
 
-print('Hello Word')
-print('Ini contoh program python')
+# PERCABANGAN : Eksekusi Melompat
+ada_susu = False
+ada_telur = True
+if ada_susu:
+    if ada_telur:
+        print('Budi membeli 1 botol susu dan 6 butir telur')
+    else:
+        print('Budi membeli 1 botol susu')
+else:
+    print('Budi tidak membeli apapun')
+
